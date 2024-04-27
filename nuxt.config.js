23eb5f8816d4f0ae26743a7d795c61fe45dd103f
dnwndalsrl <dnwndwlsqja@gmail.com>
2024-04-27 16:29:07 +0900

    css 세팅

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,11 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['element-ui/lib/theme-chalk/index.css'],
+  css: [
+    'element-ui/lib/theme-chalk/index.css',
+    '@/assets/reset.css',
+    '@/assets/common.scss',
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ['@/plugins/element-ui'],
