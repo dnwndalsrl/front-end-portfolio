c29b461060e0d6cf7834961bbbb718ffe19e187f
dnwndalsrl <dnwndwlsqja@gmail.com>
2024-05-08 12:37:03 +0900

    global 컴포넌트 추가

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="layout-container">
     <Nuxt />
-    <div class="animate__animated animate__fadeIn animate__delay-3s side-bar">
-      <div class="img-box">
-        <img src="/icons/menu.png" alt="menu" />
-      </div>
-    </div>
+    <SideBarComponent />
     <div id="cursor-dot"></div>
   </div>
 </template>
@@ -34,43 +30,6 @@ export default {
 
 div.layout-container {
   position: relative;
-  div.side-bar {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: fixed;
-    width: 100px;
-    height: 100vh;
-    background-color: #bababa;
-    top: 0;
-    right: 0;
-    @include break-sp-sm {
-      width: 100%;
-      height: 4rem;
-      justify-content: end;
-    }
-    @include break-mc {
-      width: 100%;
-      height: 4rem;
-      justify-content: end;
-    }
-    div.img-box {
-      width: 3rem;
-      cursor: pointer;
-      -webkit-tap-highlight-color: transparent !important;
-      transform: rotate(90deg);
-      @include break-sp-sm {
-        width: 2rem;
-        transform: rotate(180deg);
-        margin-right: 0.8rem;
-      }
-      @include break-mc {
-        width: 2rem;
-        transform: rotate(180deg);
-        margin-right: 0.8rem;
-      }
-    }
-  }
   #cursor-dot {
     position: fixed;
     width: 10px;
---
+++ plugins/global-components.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import SideBarComponent from '~/components/layouts/sidebar.vue'
+
+// layouts
+Vue.component('SideBarComponent', SideBarComponent)
